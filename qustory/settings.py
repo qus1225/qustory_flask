01a2bb5ef31cb6c079e0c21bf0abc88f489f839c
qus1225 <qus1225@gmail.com>
2018-06-02 02:24:10 +0900

    Set Localhost

--- qustory/settings.py
+++ qustory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!28x$-m(c+m&%y_d(gzy0&j5gtj21mu5+$f-h%m0a$h8zdh@@o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
